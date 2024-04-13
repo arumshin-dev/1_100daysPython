a6c2c4b97ddcb4ca547008be824a26994b5f52ee
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-04-13 08:50:37 +0000

    7 nesting if if

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
-print("SECURE LOGIN")
-username = input("Username > ")
-password = input("Password> ")
+  tvShow = input("What is your favorite tv show? ")
+  if tvShow == "peppa pig":
+    print("Ugh, why?")
+    faveCharacter = input("Who is your favorite character? ")
+    if faveCharacter == "daddy pig":
+      print("Right answer")
+    else:
+      print("Nah, Daddy Pig's the greatest")
+  elif tvShow == "paw patrol":
+    print("Aww, sad times")
+  else:
+    print("Yeah, that's cool and all…")
 
-if username == "mark" and password == "password":
- print("Welcome Mark!")
-elif username == "suzanne" and password == "Su74nne":
- print("Hey there Suzanne!")
-else:
- print("Go away!")
+  order = input("What would you like to order: pizza or hamburger? ")
+  if order == "hamburger":
+    print("Thank you.")
+    cheese = input("Do you want cheese?")
+    if cheese == "yes":
+      print("You got it.")
+    else: 
+      print("No cheese it is.")
+  elif order == "pizza":
+    print("Pizza coming up.")
+    toppings = input("Do you want pepperoni on that?")
+    if toppings == "yes":
+      print("We will add pepperoni.")
+  else:
+    print("Your pizza will not have pepperoni.")
 
-season = input("What is your favorite season?")
-if season == "spring":
-  print("Ah! The birds are chirping and flowers blooming.")
-elif season == "summer":
-  print("Catch some sun and cool off with a lemonade.")
-elif season == "autumn":
-  print("The leaves are changing and the air is crisp. Enjoy!")
-elif season == "winter":
-  print("Stay warm by the fire and watch the snow fall.")
-else: 
-  print("I don't know that season. Please try again.")
+  print ("Are you a superfan of 'The Big Bang Theory' or a fake fan?")
+  print()
+  print("Answer these questions to find out.")
 
-
-print("Secure Login")
-username = input("What is your username?")
-password = input("What is your password?")
-
-if username == "David" and password == "PyTh0nR*cks":
-  print("Welcome, David! You are looking nice today!")
-elif username == "Becky" and password == "Repl!t4evEr":
-  print("Hi Becky! Love your hair today!")
-elif username == "Bill" and password == "SmashTHEb*gs!":
-  print("Yo! Bill! What up?!")
-else:
-  print("You do not have access. Go away!")
+  Glasses = input("Does someone wear glasses?")
+  if Glasses == "yes":
+    print("Correct!")
+  else:
+    print("Wrong!")
+    WhoGlasses = input("And who wears glasses?")
+    if WhoGlasses == "Leonard":
+      print("You got it")
+    else:
+      print("Try again!")
