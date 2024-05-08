7da95c63fd1fc7604410fd59afc75af20477dad0
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-05-08 13:19:16 +0000

    10 math

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
-# equal
-5 == 5
+adding = 4 + 3
+print(adding)
 
-# not equal
-3 != 5
+subtraction = 8 - 9
+print(subtraction)
 
-# greater than
-5 > 3
+multiplication = 4 * 32
+print(multiplication)
 
-# greater than or equal to
-5 >= 3
+division = 50 / 5
+print(division)
 
-# less than
-3 < 5
+# a number raised to the power of some number
+# in this example we raise 5 to the power of 2
+squared = 5**2
+print(squared)
 
-# less than or equal to
-3 <= 5
-myScore = int(input("Your score: "))
-if myScore > 100000:
-  print("Winner!")
-else:
-  print("Try again 😭")
+# remainder of a division
+modulo = 15 % 4
+print(modulo)
 
-myPi = float(input("What is Pi to 3dp? "))
-if myPi == 3.142 :
-  print("Exactly!")
-else:
-  print("Try again 😭")
+# whole number division
+divisor = 15 // 2
+print(divisor)
 
-score = int(input("What was your score on the test?"))
-if score >= 80:
-  print("Not too shabby")
-elif score >= 70:
-  print("Acceptable.")
-else:
-  print("Dude, you need to study more!")
+# Solve the following problems with my code
+# Your goal is to print the solution of all 3 calculations to the screen.
 
+# multiplication
+print(3.4 * 6.8)
 
-birthYear = int(input("What year were you born?"))
-if birthYear <= 1946:
-  print("You are a Traditionalist.")
-elif birthYear >= 1947 and birthYear <= 1964:
-  print("Hey, Baby Boomer! How you doing?")
-elif birthYear >= 1965 and birthYear <= 1981:
-  print("Gen X! What's up?")
-elif birthYear >= 1982 and birthYear <= 1995:
-  print("Millenials! The age of tech!")
-elif birthYear >= 1996:
-  print("Hey, Gen Z! TikTok much?")
-else: 
-  print("Try again!")
+# division
+print(2467 / 4673)
+
+#raise 10 to the power of 2
+print(10**2)
+
+# print the remainder when 343 is divided by 4
+print(343 // 100)
+
+myBill = float(input("What was the bill?: "))
+numberOfPeople = int(input("How many people?: "))
+answer = myBill / numberOfPeople
+print("You all owe", answer)
+answer = round(answer, 2)
+
+myBill = float(input("What was the bill?: "))
+numberOfPeople = int(input("How many people?: "))
+tip = int(input("What percent tip do you want to leave: 15, 18, or 20 percent? "))
+
+bill_with_tip = tip / 100 * myBill + myBill
+bill_per_person = bill_with_tip / numberOfPeople
+final_amount = round(bill_per_person, 2)
+
+print("You all owe", final_amount)
