016afaed8573f600a726ad201fcad9437b2463c0
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-05-13 12:59:44 +0000

    11 year leap year

--- main.py
+++ main.py
@@ -1,55 +1,20 @@
-adding = 4 + 3
-print(adding)
+#"525,600 minutes"
 
-subtraction = 8 - 9
-print(subtraction)
+days_this_year = int(input("How many days are in this year?"))
 
-multiplication = 4 * 32
-print(multiplication)
+days_in_year = 365
+days_in_leapyear = 366
+hours_in_day = 24
+minutes_in_hour = 60
+seconds_in_minute = 60
 
-division = 50 / 5
-print(division)
 
-# a number raised to the power of some number
-# in this example we raise 5 to the power of 2
-squared = 5**2
-print(squared)
+result = days_in_year * hours_in_day * minutes_in_hour * seconds_in_minute
 
-# remainder of a division
-modulo = 15 % 4
-print(modulo)
+leapyear_result = days_in_leapyear * hours_in_day * minutes_in_hour * seconds_in_minute
 
-# whole number division
-divisor = 15 // 2
-print(divisor)
 
-# Solve the following problems with my code
-# Your goal is to print the solution of all 3 calculations to the screen.
-
-# multiplication
-print(3.4 * 6.8)
-
-# division
-print(2467 / 4673)
-
-#raise 10 to the power of 2
-print(10**2)
-
-# print the remainder when 343 is divided by 4
-print(343 // 100)
-
-myBill = float(input("What was the bill?: "))
-numberOfPeople = int(input("How many people?: "))
-answer = myBill / numberOfPeople
-print("You all owe", answer)
-answer = round(answer, 2)
-
-myBill = float(input("What was the bill?: "))
-numberOfPeople = int(input("How many people?: "))
-tip = int(input("What percent tip do you want to leave: 15, 18, or 20 percent? "))
-
-bill_with_tip = tip / 100 * myBill + myBill
-bill_per_person = bill_with_tip / numberOfPeople
-final_amount = round(bill_per_person, 2)
-
-print("You all owe", final_amount)
+if days_this_year == 366:
+  print("Number of seconds in a leap year are", leapyear_result)
+else:
+  print("Number of seconds in a year are", result)
