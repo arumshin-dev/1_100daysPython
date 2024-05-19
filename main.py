f0621b5b2a0f36c5a0b0f086c41c20e328d8fc49
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2024-05-19 10:30:14 +0000

    movie_ids requests

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ movie_ids = [
 for movie_id in movie_ids:
     url = f"https://nomad-movies.nomadcoders.workers.dev/movies/{movie_id}"
     response = requests.get(url)
-    movie = response.json()
-    print(movie["title"])
-    print(movie["overview"])
-    print(movie["vote_average"])
+    movie_data = response.json()
+    print(
+        "title : ",movie_data["title"],
+        "\n overview : " ,movie_data["overview"], 
+        "\n vote_average : ",movie_data["vote_average"],
+        "\n -----------------------------------------------"
+        )
