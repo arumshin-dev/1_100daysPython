9a1ab8e04705976b493f8a06da7edfebc05708a8
arumshin-dev <159020719+arumshin-dev@users.noreply.github.com>
2025-07-11 15:34:01 +0000

    Day 14 - 100 Days of Code: 2 player rock, paper, scissor!

--- main.py
+++ main.py
@@ -1,30 +1,28 @@
-print("Exam Grade Calculator")
-print()
-name_of_exam = input("Name of exam: ")
-print()
-total_score = int(input("Max. Possible Score:"))
-your_score = int(input("Your score: "))
-print()
+'''
+from getpass import getpass as input
+p1 = input("p1 mowe: r , p , or  s?")
+p2 = input("p2 mowe: r , p , or  s?")
+if p2=="r"and p1=="p"or p2=="s"and p1=="p"or p2=="p"and p1=="r":
+  print("p2 win")
+elif p1=="r"and p2=="p"or p1=="s"and p2=="p"or p1=="p"and p2=="r":
+  print("p1 win")
+else:
+  print("draw")
+'''
+from getpass import getpass as input
+import random
 
+print("Rock, Paper, Scissors, Shoot!")
+move = input("Select your move (R, P, or S): ").upper()
 
+choices = ['R', 'P', 'S']
+computer = random.choice(choices)
 
-number_score = float(your_score / total_score)
-final_number = round(number_score, 2)
-final_perc = round(float(your_score / total_score)*100, 2)
+print(f"Computer chose {computer}")
 
-print("You got",final_perc,"%")
-
-if final_number >= .90:
-  print("Your letter score is an A+")
-elif final_number >= .80 and final_number <= .89:
-  print("Your letter grade is an A-.")
-elif final_number >= .70 and final_number <= .79:
-  print("Your letter score is a B.")
-elif final_number >= .60 and final_number <= .69:
-  print("Your letter grade is a C.")
-elif final_number >= .50 and final_number <= .59:
-  print("Your letter grade is a D.")
-elif final_number <= .49:
-  print("Your letter grade is a U.")
-else: 
-  print("Try again!")
+if move == computer:
+    print("It's a tie!")
+elif (move == "R" and computer == "S") or (move == "S" and computer == "P") or (move == "P" and computer == "R"):
+    print("You win!")
+else:
+    print("You lose!")
